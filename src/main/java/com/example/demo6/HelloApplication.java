50449b70c02714a3ed00c291177215e0d1931fbd
Bilguunbilguun123 <bilguunmonkhbayar123@gmail.com>
2025-04-05 16:17:11 +0800

    1.0

--- src/main/java/com/example/demo6/HelloApplication.java
+++ src/main/java/com/example/demo6/HelloApplication.java
@@ -16,7 +16,6 @@ public class HelloApplication extends Application {
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
-
     }
     public static void main(String[] args) {
         launch();
