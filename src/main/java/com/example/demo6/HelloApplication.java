85a6611d73ea41268a0488915515f8da79bbc6ed
Bilguunbilguun123 <bilguunmonkhbayar123@gmail.com>
2025-04-05 16:00:45 +0800

    1.0

--- src/main/java/com/example/demo6/HelloApplication.java
+++ src/main/java/com/example/demo6/HelloApplication.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
         DatabaseConnection.initializeDatabase();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 700);
